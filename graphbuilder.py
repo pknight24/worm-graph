8b913543ae7c1b5c44778dbb633ee686fe1e39eb
Parker Knight <pknight24@protonmail.com>
2018-05-15 23:19:54 -0400

    Generate basic graphs

--- graphbuilder.py
+++ graphbuilder.py
@@ -1,4 +1,18 @@
-def buildGraph(adj):
+def buildGraph(root, adj):
     import networkx as nx
+
     G = nx.Graph()
+    G.add_node(root)
+    for key in adj:
+        G.add_node(key)
+        e = (root, key)
+        G.add_edge(*e)
+    return G
+
+def display(g):
+    import networkx as nx
+    import matplotlib.pyplot as plt
+
+    nx.draw(g, with_labels=True)
+    plt.show()
 
--- wormgraph.py
+++ wormgraph.py
@@ -6,7 +6,6 @@ import pandas as pd
 import sys
 
 
-
 ellista = open("ellista.txt", "r")
 
 cells = []
@@ -21,14 +20,24 @@ unique_cells = np.unique(cells)
 if (len(sys.argv) < 2):
     print("Usage:\n\twormgraph <NEURON> <NEURON>\n\tIf only one neuron is given, all connections for the given neuron will be returned.")
 else:
-    ad = adj.getAdjacencies(sys.argv[1])
-    if (len(sys.argv) == 3):
-        key = sys.argv[2]
-        if key in ad:
-            print(ad[key])
-        else:
-            print(0)
+    
+    args = sys.argv[1:]
+    if (args[0] == "-g" or args[0] == "--graph"):
+        root = args[1]
+        ad = adj.getAdjacencies(root)
+        graph = gb.buildGraph(root, ad)
+        gb.display(graph)
+
     else:
-        print(pd.Series(ad))
+        root = args[0]
+        ad = adj.getAdjacencies(root)
+        if (len(sys.argv) == 3):
+            key = args[1]
+            if key in ad:
+                print(ad[key])
+            else:
+                print(0)
+        else:
+            print(pd.Series(ad))
 
 ellista.close()
